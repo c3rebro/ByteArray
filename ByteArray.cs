3c48ed84123f01400d28d33f9b2b3c0608dfdd37
c3rebro <mail@steven-online.at>
2018-03-12 21:46:36 +0100

    + initial commit

---
+++ ByteArray.cs
@@ -0,0 +1,187 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: C3rebro
+ * Date: 12.03.2018
+ * Time: 21:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace ByteArrayHelper
+{
+
+	public class ByteArray
+	{
+		public int Length { get { return Data.Length; } }
+		
+		public byte[] Data { get; set; }
+		
+		public ByteArray()
+		{
+			
+		}
+		
+		
+		public static byte[] Or(List<byte[]> source, bool isLittleEndian = true)
+		{
+			
+			ObservableCollection<byte[]> localCopy = new ObservableCollection<byte[]>(source);
+			
+			for (int i = source.Count; i > 0; i--)
+			{
+				foreach ( byte[] b in source)
+				{
+					
+				}
+			}
+			
+			return null;
+			//localCopy.Aggregate((i, j) => i.MainListWords.Length > j.MainListWords.Length ? i : j)
+		}
+		
+
+	}
+	
+	namespace Extensions
+	{
+		/// <summary>
+		/// Description of MyClass.
+		/// </summary>
+		public static class ByteConverter
+		{
+			
+			public static byte[] ConvToLittleEndian(byte[] bigEndian)
+			{
+				if (BitConverter.IsLittleEndian)
+					Array.Reverse(bigEndian);
+				
+				return bigEndian;
+			}
+			
+			public static byte Reverse(byte x)
+			{
+				byte y = 0;
+				for (byte i = 0; i < 8; ++i)
+				{
+					y <<= 1;
+					y |= (byte)(x & 1);
+					x >>= 1;
+				}
+				return y;
+			}
+			
+			public static int GetByteCount(string hexString)
+			{
+				int numHexChars = 0;
+				char c;
+				// remove all none A-F, 0-9, characters
+				for (int i = 0; i < hexString.Length; i++)
+				{
+					c = hexString[i];
+					if (IsHexDigit(c))
+						numHexChars++;
+				}
+				// if odd number of characters, discard last character
+				if (numHexChars % 2 != 0)
+				{
+					numHexChars--;
+				}
+				return numHexChars / 2; // 2 characters per byte
+			}
+
+			public static byte[] GetBytes(string hexString, out int discarded)
+			{
+				discarded = 0;
+				string newString = "";
+				char c;
+				// remove all none A-F, 0-9, characters
+				for (int i = 0; i < hexString.Length; i++)
+				{
+					c = hexString[i];
+					if (IsHexDigit(c))
+						newString += c;
+					else
+						discarded++;
+				}
+				// if odd number of characters, discard last character
+				if (newString.Length % 2 != 0)
+				{
+					discarded++;
+					newString = newString.Substring(0, newString.Length - 1);
+				}
+
+				int byteLength = newString.Length / 2;
+				byte[] bytes = new byte[byteLength];
+				string hex;
+				int j = 0;
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					hex = new String(new Char[] { newString[j], newString[j + 1] });
+					bytes[i] = HexToByte(hex);
+					j = j + 2;
+				}
+				return bytes;
+			}
+
+			public static string HexToString(byte[] bytes)
+			{
+				string hexString = "";
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					hexString += bytes[i].ToString("X2");
+				}
+				return hexString;
+			}
+
+			public static string HexToString(byte bytes)
+			{
+				string hexString = "";
+				{
+					hexString += bytes.ToString("X2");
+				}
+				return hexString;
+			}
+
+			public static bool IsInHexFormat(string hexString)
+			{
+				bool hexFormat = true;
+
+				foreach (char digit in hexString)
+				{
+					if (!IsHexDigit(digit))
+					{
+						hexFormat = false;
+						break;
+					}
+				}
+				return hexFormat;
+			}
+
+			public static bool IsHexDigit(Char c)
+			{
+				int numChar;
+				int numA = Convert.ToInt32('A');
+				int num1 = Convert.ToInt32('0');
+				c = Char.ToUpper(c);
+				numChar = Convert.ToInt32(c);
+				if (numChar >= numA && numChar < (numA + 6))
+					return true;
+				if (numChar >= num1 && numChar < (num1 + 10))
+					return true;
+				return false;
+			}
+
+			private static byte HexToByte(string hex)
+			{
+				if (hex.Length > 2 || hex.Length <= 0)
+					throw new ArgumentException("hex must be 1 or 2 characters in length");
+				byte newByte = byte.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+				return newByte;
+			}
+		}
+	}
+
+}
